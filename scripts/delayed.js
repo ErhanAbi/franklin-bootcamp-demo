03ea038b3e1872a80dc7f5e2a0b5d6fbe43cb336
Erhan Abibula <abibula@adobe.com>
2023-03-06 09:48:36 +0200

    work on indexing

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,8 +1,19 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM } from "./lib-franklin.js";
 
 // Core Web Vitals RUM collection
-sampleRUM('cwv');
+sampleRUM("cwv");
 
 // add more delayed functionality here
 // martech
+
+fetch("/form-data", {
+  method: "POST",
+  data: JSON.stringify({
+    data: {
+      title: "This my title",
+      adventure: "A lot of adventure",
+      date: new Date(),
+    },
+  }),
+});
